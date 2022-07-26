11aecf314214139a5f6b71e2ae9d0d0f7b6fc0a6
brayanblanco <brayan.martin.blanco@gmail.com>
2022-07-26 01:19:42 +0100

    Remove database settings from public settings.

--- movieraptor/settings.py
+++ movieraptor/settings.py
@@ -69,18 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'movieraptor.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
